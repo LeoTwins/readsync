768b2b767555fd97f4782c9e3714f51429cc85d5
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2025-08-08 20:16:00 +0900

    feat: MacOS開発環境構築

---
+++ macos/ReadSync/ReadSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadSyncApp.swift
+//  ReadSync
+//
+//  Created by Kazuma Sakaguchi on 2025/08/08.
+//
+
+import SwiftUI
+
+@main
+struct ReadSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
